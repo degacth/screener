0d44041847c8460618ecb5ae51765b903a3f6378
Degtarov Alexander <aleksandr.degtyarev@swisstok.ru>
2017-04-21 09:02:47 +0300

    add click command

--- __main__.py
+++ __main__.py
@@ -1,2 +1,16 @@
+import click
+
+
 class Screener:
-    pass
+    def __init__(self, config_name):
+        print(config_name)
+
+
+@click.command()
+@click.option('--config', '-c', prompt='You must input config', help='config for screener subclass of base config')
+def run(config):
+    Screener(config)
+
+
+if __name__ == '__main__':
+    run()
